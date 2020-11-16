b7017357289cd9ff00c1b1366ec7139392a1cc15
Kyle <xikkzz@gmail.com>
2020-11-16 10:04:07 -0500

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BucketListComponent } from './bucket-list/bucket-list.component';
+import { EventsListComponent } from './events-list/events-list.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'favorites', component: BucketListComponent },
+  { path: 'event-list', component: EventsListComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }, // redirect to `first-component`
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
